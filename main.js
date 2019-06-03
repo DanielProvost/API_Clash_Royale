71b775055e918ea4f50fbb8fce65e6775e5522ea
Maegrith <dprovost67@gmail.com>
2019-06-03 09:20:53 +0200

    interrogation avec ajax

--- main.js
+++ main.js
@@ -10,39 +10,45 @@ $(function () {
         "headers": {
             "auth": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjY0MSwiaWRlbiI6IjUyMzU0MTIxMjkzMDc2ODkwNyIsIm1kIjp7InVzZXJuYW1lIjoiTWFlZ3JpdGgiLCJrZXlWZXJzaW9uIjozLCJkaXNjcmltaW5hdG9yIjoiMTQ0MiJ9LCJ0cyI6MTU1OTI5MzM1NDY2N30.NcnfY3tw-lKwMmpzqOd5SygmZpgb4fDecvv2zfxBmpw"
         }
-    }
+    };
 
     $.ajax(settings).done(function (response) {
 
         for(var i=0; i<10;i++) {
-
-            $('#liste').append("<tr><td>"+(i+1)+"</td><td>" + "<a href='' data-id = "+response[i].tag+" >"+response[i].name +"</a></td>" + "<td>" + response[i].trophies + "</td></tr>")
+            $('#liste').append("<tr><td>"+(i+1)+"</td><td>" + "<a href='#'  data-tag= "+response[i].tag+" >"+response[i].name +"</a></td>" + "<td>" + response[i].trophies + "</td></tr>")
         }
-    });
-
-    // var select = document.querySelectorAll('a')
-    // select.addEventListener('click',function(){
-    //
-   
-
-    var settings1 = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://api.royaleapi.com/players/8RC8UUYP",
-        "method": "GET",
-        "headers": {
-            "auth": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjY0MSwiaWRlbiI6IjUyMzU0MTIxMjkzMDc2ODkwNyIsIm1kIjp7InVzZXJuYW1lIjoiTWFlZ3JpdGgiLCJrZXlWZXJzaW9uIjozLCJkaXNjcmltaW5hdG9yIjoiMTQ0MiJ9LCJ0cyI6MTU1OTI5MzM1NDY2N30.NcnfY3tw-lKwMmpzqOd5SygmZpgb4fDecvv2zfxBmpw"
-        }
-    }
-
-    $.ajax(settings1).done(function (response) {
-        console.log(response.games)
-        console.log('le joueur a '+ response.games.wins +' de victoires')
 
 
-    });
-
-    // })
+        $('a').on('click',function(){
 
 
-})
+            var data = $(this).attr("data-tag");
+            var settings1 = {
+                "async": true,
+                "crossDomain": true,
+                "url": "https://api.royaleapi.com/players/"+ data,
+                "method": "GET",
+                "headers": {
+                    "auth": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjY0MSwiaWRlbiI6IjUyMzU0MTIxMjkzMDc2ODkwNyIsIm1kIjp7InVzZXJuYW1lIjoiTWFlZ3JpdGgiLCJrZXlWZXJzaW9uIjozLCJkaXNjcmltaW5hdG9yIjoiMTQ0MiJ9LCJ0cyI6MTU1OTI5MzM1NDY2N30.NcnfY3tw-lKwMmpzqOd5SygmZpgb4fDecvv2zfxBmpw"
+                }
+            };
+
+            $.ajax(settings1).done(function (response) {
+
+                console.log(response.currentDeck)
+
+                $('#liste1').empty().append("<tr><td>" + response.games.winsPercent + "</td><td>" + response.games.wins + "</td><td>"+ response.games.total + "</td></tr>");
+                $('#name').empty().append(response.name + '<br>')
+
+            });
+
+        });
+
+    })
+
+
+
+
+
+});
+
--- readMe.txt
+++ readMe.txt
@@ -1,12 +1,11 @@
 Document de présentation du projet API_Clash_Royale
 
 Dans ce projet nous interrongeons l' API officielle du jeu de Supercell Clash Royale afin
-de connaître les 10 meilleurs joueurs par pays(Européens).
+de connaître les 10 meilleurs joueurs au monde.
 
 Donnéees :
 - récupérer les données API via Php, les enregistrer en base de donnée
 - interroger la bdd via Javascript getJson();
 
-Objectif, interface graphique simple ou on sélection un pays dans une liste, et un tableau
-contenant le player's ranking s'affiche et/ou si possible afficher les joueurs par pays
-via une carte Google Api;
+Objectif, interface graphique simple ou  un tableau contenant le player's ranking s'affiche. Au click
+s'affiche les statistiques par joueur et ses cartes favorites.
